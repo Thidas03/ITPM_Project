a5b5c8e698d8e17b750990d2eae12c11dc69db06
IT23621374 <IT23621374@my.sliit.lk>
2026-03-20 14:02:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
+
+const feedbackRoutes = require('./routes/feedbackRoutes');
+const availabilityRoutes = require('./routes/availabilityRoutes');
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/feedback', feedbackRoutes);
+app.use('/api/availability', availabilityRoutes);
 
 app.get('/', (req, res) => {
   res.send("API Running...");
